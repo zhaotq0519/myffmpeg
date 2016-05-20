8c149349d98c991cb5749bf57d8b51258fdd8ab1
zhaotq <zhaotq0519@gmail.com>
2016-05-20 18:05:22 +0800

    add tutorial01.c

--- ffmpeg/tutorial/main.c
+++ ffmpeg/tutorial/main.c
@@ -4,8 +4,13 @@
 #include "swscale.h"
 
 
+#if 0
 int main(int argc, char* argv[])
 {
+	AVFormatContext* pFormatCtx = NULL;
+
+	int ret = 0;
+
 	if(argc < 2)
 	{
 		printf("Please provide a file.\n");
@@ -14,5 +19,12 @@ int main(int argc, char* argv[])
 
 	av_register_all();
 
+	ret = avformat_open_input(&pFormatCtx,argv[1],NULL,NULL);
+	if(0!=ret)
+	{
+		return -1;
+	}
+
 	return 0;
 }
+#endif
